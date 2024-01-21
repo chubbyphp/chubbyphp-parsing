fc682b53e020d5e4be64a6487ae6a4fbcc07998f
Dominik Zogg <dominik.zogg@hotmail.com>
2024-01-21 11:00:19 +0100

    Better nested testing to get 100% coverage event with mutations

--- tests/Unit/ParserErrorExceptionTest.php
+++ tests/Unit/ParserErrorExceptionTest.php
@@ -76,48 +76,62 @@ final class ParserErrorExceptionTest extends TestCase
 
     public function testNested(): void
     {
+        $i = 1;
+
         $exception = (new ParserErrorException())
-            ->addError('This is a error message', 'field1')
-            ->addError('This is a error message', 'field2')
-            ->addError('This is a error message', 'field3')
+            ->addError('error: '.$i++, 'field1')
+            ->addError('error: '.$i++, 'field2')
+            ->addError('error: '.$i++, 'field3')
             ->addParserErrorException(
                 (new ParserErrorException())
-                    ->addError('This is a error message', 'field1')
-                    ->addError('This is a error message', 'field2')
+                    ->addError('error: '.$i++, 'field1')
+                    ->addError('error: '.$i++, 'field2')
                     ->addParserErrorException(
                         (new ParserErrorException())
-                            ->addError('This is a error message', 'field1')
-                            ->addError('This is a error message', 'field2'),
+                            ->addError('error: '.$i++, 'field1')
+                            ->addError('error: '.$i++, 'field2'),
                         'field3'
                     ),
                 'field4'
             )
             ->addParserErrorException(
                 (new ParserErrorException())
-                    ->addError('This is a error message', 'field1')
-                    ->addError('This is a error message', 'field2')
+                    ->addError('error: '.$i++, 'field1')
+                    ->addError('error: '.$i++, 'field2')
                     ->addParserErrorException(
                         (new ParserErrorException())
-                            ->addError('This is a error message', 'field1')
-                            ->addError('This is a error message', 'field2'),
+                            ->addError('error: '.$i++, 'field1')
+                            ->addError('error: '.$i++, 'field2'),
                         'field3'
                     ),
                 'field4'
+            )
+            ->addError('error: '.$i++, 'field1')
+            ->addParserErrorException(
+                (new ParserErrorException())
+                    ->addError('error: '.$i++)
+                    ->addError('error: '.$i++)
+                    ->addParserErrorException(
+                        (new ParserErrorException())
+                            ->addError('error: '.$i++)
+                    ),
+                'field5'
             )
         ;
 
         self::assertEquals([
-            'field1' => ['This is a error message'],
-            'field2' => ['This is a error message'],
-            'field3' => ['This is a error message'],
+            'field1' => ['error: 1', 'error: 12'],
+            'field2' => ['error: 2'],
+            'field3' => ['error: 3'],
             'field4' => [
-                'field1' => ['This is a error message', 'This is a error message'],
-                'field2' => ['This is a error message', 'This is a error message'],
+                'field1' => ['error: 4', 'error: 8'],
+                'field2' => ['error: 5', 'error: 9'],
                 'field3' => [
-                    'field1' => ['This is a error message', 'This is a error message'],
-                    'field2' => ['This is a error message', 'This is a error message'],
+                    'field1' => ['error: 6', 'error: 10'],
+                    'field2' => ['error: 7', 'error: 11'],
                 ],
             ],
+            'field5' => ['error: 13', 'error: 14', 'error: 15'],
         ], $exception->getErrors());
         self::assertTrue($exception->hasError());
     }
