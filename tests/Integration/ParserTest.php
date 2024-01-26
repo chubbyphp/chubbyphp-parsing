5ce06652c2f21aababb4007a730190f44578e409
Dominik Zogg <dominik.zogg@hotmail.com>
2024-01-26 20:17:30 +0100

    Add the new schemas to the integration test

--- tests/Integration/ParserTest.php
+++ tests/Integration/ParserTest.php
@@ -9,6 +9,14 @@ use Chubbyphp\Parsing\ParserErrorException;
 use Chubbyphp\Parsing\Schema\SchemaInterface;
 use PHPUnit\Framework\TestCase;
 
+enum BackedSuit: string
+{
+    case Hearts = 'H';
+    case Diamonds = 'D';
+    case Clubs = 'C';
+    case Spades = 'S';
+}
+
 /**
  * @internal
  *
@@ -26,6 +34,7 @@ final class ParserTest extends TestCase
                     "test1",
                     "test2"
                 ],
+                "backedEnum": "D",
                 "bool": true,
                 "dateTime": {
                     "date": "2024-01-20 09:15:00.000000",
@@ -43,13 +52,22 @@ final class ParserTest extends TestCase
                 "object": {
                     "string": "test"
                 },
+                "record": {
+                    "key1": "value1",
+                    "key2": "value2"
+                },
                 "string": "test",
+                "tuple": [
+                    41.5,
+                    8.5
+                ],
                 "union": 42
             }
             EOD;
 
         self::assertEquals($ouputAsJson, json_encode($schema->parse([
             'array' => ['test1', 'test2'],
+            'backedEnum' => 'D',
             'bool' => true,
             'dateTime' => new \DateTimeImmutable('2024-01-20T09:15:00+00:00'),
             'discriminatedUnion' => ['literal' => 'type1', 'string' => 'test'],
@@ -57,7 +75,9 @@ final class ParserTest extends TestCase
             'int' => 5,
             'literal' => 1337,
             'object' => ['string' => 'test'],
+            'record' => ['key1' => 'value1', 'key2' => 'value2'],
             'string' => 'test',
+            'tuple' => [41.5, 8.5],
             'union' => 42,
         ]), JSON_PRETTY_PRINT));
     }
@@ -76,6 +96,15 @@ final class ParserTest extends TestCase
                     [
                         'code' => 'array.type',
                         'template' => 'Type should be "array", "{{given}}" given',
+                        'variables' => [
+                            'given' => 'NULL',
+                        ],
+                    ],
+                ],
+                'backedEnum' => [
+                    [
+                        'code' => 'backedEnum.type',
+                        'template' => 'Type should be "int|string", "{{given}}" given',
                         'variables' => [
                             'given' => 'NULL',
                         ],
@@ -144,10 +173,28 @@ final class ParserTest extends TestCase
                         ],
                     ],
                 ],
+                'record' => [
+                    [
+                        'code' => 'record.type',
+                        'template' => 'Type should be "array", "{{given}}" given',
+                        'variables' => [
+                            'given' => 'NULL',
+                        ],
+                    ],
+                ],
                 'string' => [
                     [
                         'code' => 'string.type',
                         'template' => 'Type should be "string", "{{given}}" given',
+                        'variables' => [
+                            'given' => 'NULL',
+                        ],
+                    ],
+                ],
+                'tuple' => [
+                    [
+                        'code' => 'tuple.type',
+                        'template' => 'Type should be "array", "{{given}}" given',
                         'variables' => [
                             'given' => 'NULL',
                         ],
@@ -183,6 +230,7 @@ final class ParserTest extends TestCase
 
         $object = new class() {
             public array $array;
+            public BackedSuit $backedEnum;
             public bool $bool;
             public \DateTimeImmutable $dateTime;
             public object $discriminatedUnion;
@@ -190,7 +238,9 @@ final class ParserTest extends TestCase
             public int $int;
             public bool|float|int|string $literal;
             public object $object;
+            public object $record;
             public string $string;
+            public array $tuple;
             public int|string $union;
         };
 
@@ -198,6 +248,7 @@ final class ParserTest extends TestCase
 
         return $p->object([
             'array' => $p->array($p->string()),
+            'backedEnum' => $p->backedEnum(BackedSuit::class),
             'bool' => $p->bool(),
             'dateTime' => $p->dateTime(),
             'discriminatedUnion' => $p->discriminatedUnion([
@@ -207,7 +258,9 @@ final class ParserTest extends TestCase
             'int' => $p->int(),
             'literal' => $p->literal(1337),
             'object' => $p->object(['string' => $p->string()]),
+            'record' => $p->record($p->string()),
             'string' => $p->string(),
+            'tuple' => $p->tuple([$p->float(), $p->float()]),
             'union' => $p->union([$p->string(), $p->int()]),
         ], $object::class);
     }
