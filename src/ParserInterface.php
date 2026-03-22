17ba81b805217c4ca6aeae6d48b7164b208ce15b
Dominik Zogg <dominik.zogg@ikmail.com>
2026-03-22 16:57:20 +0100

    fix interface phpdoc

--- src/ParserInterface.php
+++ src/ParserInterface.php
@@ -23,9 +23,9 @@ use Chubbyphp\Parsing\Schema\TupleSchema;
 use Chubbyphp\Parsing\Schema\UnionSchema;
 
 /**
- * @method AssocSchema assoc(array<string, SchemaInterface> $fieldNameToSchema)
- * @method ConstSchema const(bool|float|int|string $const)
- * @method ObjectSchema object(array<string, SchemaInterface> $fieldNameToSchema, class-string $classname = \stdClass::class, bool $construct = false)
+ * @method AssocSchema  assoc(array<string, SchemaInterface> $fieldNameToSchema)
+ * @method ConstSchema  const(bool|float|int|string $const)
+ * @method ObjectSchema object(array<string, SchemaInterface> $fieldNameToSchema, class-string $classname = 'stdClass', bool $construct = false)
  */
 interface ParserInterface
 {
